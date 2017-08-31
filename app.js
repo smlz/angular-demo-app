90bc07c50c06d4610110c343fdc450e6f432dd15
Marco Schmalz <marco@schess.ch>
2017-08-31 16:30:22 +0200

    lineare funktionen generieren

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ var app = angular.module('app', []);
 
 app.controller('namensAnzeigeCtrl', function () {
   var vm = this;
-  vm.name = 'Marco';
+  console.log(vm);
   vm.eingabe = [-3, -2, -1, 0, 1, 2, 3, 4];
-  vm.action = function () {
-    vm.name = vm.name.split("").reverse().join("");
-  };
-  vm.f = function (x) {
-    return vm.m * x + vm.b;
+  vm.m = 1;
+  vm.b = 0;
+
+  for (var x of vm.eingabe){
+    console.log(x)
   }
+
+  vm.f = function (x) { return x; };
+
+  var generateLinearFunction = function(m, b) {
+    var f = function(x) {
+      return m * x + b;
+    };
+    return f;
+  }
+  vm.go = function() {
+    vm.f = generateLinearFunction(vm.m, vm.b);
+  };
 });
