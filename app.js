9b3b4643f6bf97b419180dac5a80b97737949cc8
Marco Schmalz <marco@schess.ch>
2017-08-31 14:24:59 +0200

    lineare funktion

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+"use strict"
+
 var app = angular.module('app', []);
 
 app.controller('namensAnzeigeCtrl', function () {
   var vm = this;
   vm.name = 'Marco';
+  vm.eingabe = [-3, -2, -1, 0, 1, 2, 3, 4];
   vm.action = function () {
     vm.name = vm.name.split("").reverse().join("");
   };
+  vm.f = function (x) {
+    return vm.m * x + vm.b;
+  }
 });
